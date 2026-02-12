0cc2582e8ec27656561851e5a62f1f3d4d6c4e5d
pt545 <pt545@exeter.ac.uk>
2026-02-12 14:59:40 +0000

    completed part 2

--- Rectangle.java
+++ Rectangle.java
@@ -7,8 +7,10 @@ public class Rectangle {
   public double originX = 0.0;
   public double originY = 0.0;
 
+
   // 1 static attributes
   public static final int NUMBER_OF_SIDES = 4;
+
 
   // main constructor
   public Rectangle(double width, double height, double originX, double originY) {
@@ -16,6 +18,7 @@ public class Rectangle {
     this.height = height;
     this.originX = originX;
     this.originY = originY;
+
   }
 
   public Rectangle(double width, double height) {
@@ -42,4 +45,50 @@ public class Rectangle {
     return 2 * (width + height);
   }
   
+  public void scale(double scaleX,double scaleY){
+    width *= scaleX;
+    height *= scaleY;
+  }
+
+  public void scale(double factor){
+    width *= factor;
+    height *= factor;
+  }
+
+
+  public boolean isOverlappedWith(Rectangle r){
+    double upperYt = this.originY + (this.height/2);
+    double lowerYt = this.originY - (this.height/2);
+    double rightXt = this.originX + (this.width/2);
+    double leftXt = this.originX - (this.width/2);
+
+    double upperYr = r.originY + (r.height/2);
+    double lowerYr = r.originY - (r.height/2);
+    double rightXr = r.originX + (r.width/2);
+    double leftXr = r.originX - (r.width/2);
+
+    if(upperYt < lowerYr || upperYr < lowerYt) return false;
+    if(rightXt < leftXr || rightXr < leftXt) return false;
+
+    return true;
+  }
+
+  public static boolean areOverlapping(Rectangle r1, Rectangle r2){
+    return r1.isOverlappedWith(r2);
+  }
+
+  public double calcRatio(){
+    double res1 = width / height;
+    double res2 = width * (1/height);
+
+    
+    if(Math.abs(res1 - res1) > 0.001) return -1;
+
+    return width / height;
+  } 
+
+  public boolean isSquare(){
+    return height == width;
+  }
+
 }
--- RectangleApp.java
+++ RectangleApp.java
@@ -36,7 +36,36 @@ public class RectangleApp {
 		Rectangle myRect3 = new Rectangle(); 
 		System.out.println("Width: "+myRect3.width+", Height: "+myRect3.height);
 		System.out.println("Origin: "+myRect3.originX+","+myRect3.originY);
-		 
+		
+
+
+
+		myRect1.scale(0.5); // applies 0.5 scale to both x and y, changing width to 8, height to 4
+
+ 		myRect2.scale(1,3); // should change height to 24 with width unchanged
+
+ 		myRect3.scale(15,10); // should scale to width 15, height 10
+
+		//System.out.println("myRect1 area: " + myRect1.getArea());
+		//System.out.println("myRect2 area: " + myRect2.getArea());
+		//System.out.println("myRect3 area: " + myRect3.getArea());
+		System.out.println("myRect1 Width: "+myRect1.width+", Height: "+myRect1.height);
+		System.out.println("myRect2 Width: "+myRect2.width+", Height: "+myRect2.height);
+		System.out.println("myRect3 Width: "+myRect3.width+", Height: "+myRect3.height);
+
+		System.out.println();
+		
+		Rectangle myRect4 = new Rectangle(30.0, 5.0, 10, 10); 
+		Rectangle myRect5 = new Rectangle(50.0, 20.0, 0, 0); 
+		Rectangle myRect6 = new Rectangle(20.0, 40.0, 500, 500); 
+
+		// myRect4 overlaps myRect5 so these should show as true
+		System.out.println( "myRect4 overlaps myRect5: " + myRect4.isOverlappedWith(myRect5) ) ; 
+		System.out.println( "myRect5 overlaps myRect4: " + myRect5.isOverlappedWith(myRect4) ) ;
+
+		// myRect4 does not overlap myRect6 so these should show as false
+		System.out.println( "myRect4 overlaps myRect6: " + myRect4.isOverlappedWith(myRect6) ) ;
+		System.out.println( "myRect6 overlaps myRect4: " + myRect6.isOverlappedWith(myRect4) ) ;
 
 	}
 }
